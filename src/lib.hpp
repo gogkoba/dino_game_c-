c7262665378de07e49d9b7d336df3f21a64f2fb3
gogkoba <73553041+gogkoba@users.noreply.github.com>
2021-06-06 01:34:41 +0300

    Update lib.hpp

--- src/lib.hpp
+++ src/lib.hpp
@@ -1,3 +1,25 @@
 #pragma once
+#include <raylib-cpp.hpp>
 
-extern int not_random();
+extern int form; // variable for determining the running posture
+
+struct Player { // creating a player structure
+    Vector2 position;
+    float speed;
+    bool canJump;
+    int dead;
+};
+
+struct EnvItem { // create the structure of the environment
+    Rectangle rect;
+    Color color;
+    float speed;
+};
+
+struct Testinf { // create the structure of the environment
+    int space;
+    int down;
+};
+
+extern void Playernow(Player *player, EnvItem *envItems, int envItemsLength,
+                      float delta, Testinf test);
