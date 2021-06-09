4c67e58e26b4df59d3114101d489d284c65b0396
gogkoba <73553041+gogkoba@users.noreply.github.com>
2021-06-09 12:33:54 +0300

    Update lib.hpp

--- src/lib.hpp
+++ src/lib.hpp
@@ -1,25 +1,44 @@
 #pragma once
 #include <raylib-cpp.hpp>
 
-extern int form; // variable for determining the running posture
+extern int form; ///< variable for determining the running posture
 
-struct Player { // creating a player structure
-    Vector2 position;
-    float speed;
-    bool canJump;
-    int dead;
+/**
+ * structure storing user data
+ */
+struct Player {       // creating a player structure
+    Vector2 position; ///< player location
+    float speed;      ///< player speed (vertical)
+    bool canJump;     ///< variable allowing to jump
+    int dead;         ///< player touched the object
 };
 
-struct EnvItem { // create the structure of the environment
-    Rectangle rect;
-    Color color;
-    float speed;
+/**
+ * structure storing objects data
+ */
+struct EnvItem {    // create the structure of the environment
+    Rectangle rect; ///< location and dimensions
+    Color color;    ///< color
+    float speed;    ///< speed (horizontal)
 };
 
+/**
+ * structure for testing
+ */
 struct Testinf { // create the structure of the environment
-    int space;
-    int down;
+    int space;   ///< parameter responsible for simulating pressing space
+    int down;    ///< parameter responsible for simulating pressing down
 };
-
+/**
+ * the function in which the states of the
+ * player and the environment are determined,
+ * their position changes.
+ * \param player to work with the player's position
+ * \param envItems to work with the position of objects
+ * \param envItemsLength number of objects
+ * \param delta to measure time
+ * \param test for testing
+ * \
+ */
 extern void Playernow(Player *player, EnvItem *envItems, int envItemsLength,
                       float delta, Testinf test);
